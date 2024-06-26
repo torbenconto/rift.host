0fce46bd1fab0bff313fa16a14628fde100d5e10
torbenconto <torbenmconto@gmail.com>
2024-06-26 16:43:04 -0400

    update hosted sites

--- sites.js
+++ sites.js
@@ -5,4 +5,16 @@ export const sites = [
         "statusurl": "https://plutus.rift.host/ping",
         "source": "https://github.com/torbenconto/plutus-api"
     },
+    {
+        "sub": "links.rift.host",
+        "name": "link shortener",
+        "statusurl": "https://links.rift.host/",
+        "source": "https://github.com/torbenconto/simple-link-shortener"
+    },
+    {
+        "sub": "l.rift.host",
+        "name": "link shortener mirror",
+        "statusurl": "https://l.rift.host/",
+        "source": "https://github.com/torbenconto/simple-link-shortener"
+    }
 ]
