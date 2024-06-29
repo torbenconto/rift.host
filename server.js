864fe9b1f9cafc555eba870d7d7db0b7b51fa9b6
torbenconto <torbenmconto@gmail.com>
2024-06-29 14:53:38 -0400

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'public/index.html'));
